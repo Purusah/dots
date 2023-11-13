64730142288ed1a005f133a92b2f8bd9426571d8
Purusah <bohaichuk.v@gmail.com>
2023-11-13 21:41:22 +0200

    nvim: fix error during tab adding

--- .config/nvim/lua/purusah/plugins/tabby.lua
+++ .config/nvim/lua/purusah/plugins/tabby.lua
@@ -4,18 +4,15 @@ return {
   config = function()
     require('tabby.tabline').use_preset('active_wins_at_tail', {
       theme = {
-        fill = 'TabLineFill',       -- tabline background
-        head = 'TabLine',           -- head element highlight
+        fill = 'TabLineFill',   -- tabline background
+        head = 'TabLine',       -- head element highlight
         current_tab = 'TabLineSel', -- current tab label highlight
-        tab = 'TabLine',            -- other tab label highlight
-        win = 'TabLine',            -- window highlight
-        tail = 'TabLine',           -- tail element highlight
+        tab = 'TabLine',        -- other tab label highlight
+        win = 'TabLine',        -- window highlight
+        tail = 'TabLine',       -- tail element highlight
       },
-      nerdfont = true,              -- whether use nerdfont
-      lualine_theme = nil,          -- lualine theme name
-      tab_name = {
-        name_fallback = 'function({tabid}), return a string',
-      },
+      nerdfont = true,          -- whether use nerdfont
+      lualine_theme = nil,      -- lualine theme name
       buf_name = {
         mode = "'unique'|'relative'|'tail'|'shorten'",
       },
