24311438ded2984921f9234781f84b9631ba1a31
Purusah <bohaichuk.v@gmail.com>
2023-11-21 10:57:12 +0200

    nvim: remove rust-tool plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,13 +15,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "rust-lang/rust.vim",
-    config = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
-
   require("purusah.plugins.comment"),
   require("purusah.plugins.completion"),
   require("purusah.plugins.conform"),
@@ -37,13 +30,6 @@ require("lazy").setup({
   require("purusah.plugins.theme-tokyonight"),
   require("purusah.plugins.treesitter"),
   require("purusah.plugins.which_key"),
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'purusah.plugins' },
 }, {})
 
