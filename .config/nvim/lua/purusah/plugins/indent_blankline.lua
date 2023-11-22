6b23a725a076452f2c711b06995b6531866b83bb
Purusah <bohaichuk.v@gmail.com>
2023-11-22 21:02:17 +0200

    nvim: make indent blankline plugin work

--- .config/nvim/lua/purusah/plugins/indent_blankline.lua
+++ .config/nvim/lua/purusah/plugins/indent_blankline.lua
@@ -1,21 +1,21 @@
 return {
-
-  -- Add indentation guides even on blank lines
   "lukas-reineke/indent-blankline.nvim",
-  -- Enable `lukas-reineke/indent-blankline.nvim`
-  -- See `:help indent_blankline.txt`
-  opts = {
-    char = "┊",
-    show_trailing_blankline_indent = false,
-
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-  },
+  main = "ibl",
   config = function()
-    vim.opt.list = true
-    -- vim.opt.listchars:append "space:⋅"
-    -- vim.opt.listchars:append "eol:↴"
+    local highlight = {
+      "CursorColumn",
+      "Whitespace",
+    }
+
+    require("ibl").setup({
+      indent = { highlight = highlight, char = "" },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+      scope = { enabled = false },
+    })
+    -- TODO: https://github.com/lukas-reineke/indent-blankline.nvim#rainbow-delimitersnvim-integration
   end,
 }
 
