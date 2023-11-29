1442919278bcda9bd7564014e4e0533fb6a2fd69
Purusah <bohaichuk.v@gmail.com>
2023-11-29 10:05:36 +0200

    nvim: specify a list of formatters for js/ts react files

--- .config/nvim/lua/purusah/plugins/conform.lua
+++ .config/nvim/lua/purusah/plugins/conform.lua
@@ -18,7 +18,9 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       javascript = { { "prettier", "deno_fmt" } }, -- a sub-list to run only the first available formatter
+      javascriptreact = { { "prettier", "deno_fmt" } }, -- a sub-list to run only the first available formatter
       typescript = { { "prettier", "eslint", "deno_fmt" } },
+      typescriptreact = { { "prettier", "eslint", "deno_fmt" } },
       rust = { { "rustfmt" } },
       ["_"] = { "trim_whitespace" }, -- run formatters on filetypes that don't have other formatters configured
       -- ["*"] = { "codespell" }, -- run formatters on all filetypes
