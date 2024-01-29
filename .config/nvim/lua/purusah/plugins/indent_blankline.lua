f0145c5819158a6e4a7702a32f73e38dceccc742
Purusah <bohaichuk.v@gmail.com>
2024-01-29 12:22:44 +0200

    nvim: hide vertical indent lines

--- .config/nvim/lua/purusah/plugins/indent_blankline.lua
+++ .config/nvim/lua/purusah/plugins/indent_blankline.lua
@@ -1,22 +1,9 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  config = function()
-    local highlight = {
-      "CursorColumn",
-      "Whitespace",
-    }
-
-    require("ibl").setup({
-      indent = { highlight = highlight, char = "" },
-      whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-      },
-      scope = { enabled = false },
-    })
-    -- TODO: https://github.com/lukas-reineke/indent-blankline.nvim#rainbow-delimitersnvim-integration
-  end,
+  opts = {
+    scope = { enabled = false },
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
