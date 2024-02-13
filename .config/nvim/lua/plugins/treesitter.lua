585f852e32d6b0482e0075048b1a8a18fd2c9b17
Purusah <bohaichuk.v@gmail.com>
2024-02-13 23:28:07 +0200

    nvim: update treesitter config with context dependency

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,35 @@
 return {
-  -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
-
   dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      opts = { max_lines = 3 },
+    },
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all"
-      ensure_installed = { "javascript", "typescript", "lua", "rust" },
+      ensure_installed = {
+        "bash",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "markdown",
+        "rust",
+        "toml",
+        "tsx",
+        "typescript",
+      },
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
-
-      -- List of parsers to ignore installing (for "all")
-      -- ignore_install = { "javascript" },
-
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
       indent = {
         enable = true,
       },
-
       highlight = {
-        -- `false` will disable the whole extension
         enable = true,
 
         -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
@@ -59,14 +59,6 @@ return {
 -- -- [[ Configure Treesitter ]]
 -- -- See `:help nvim-treesitter`
 -- require('nvim-treesitter.configs').setup {
---   -- Add languages to be installed here that you want installed for treesitter
---   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
---
---   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
---   auto_install = false,
---
---   highlight = { enable = true },
---   indent = { enable = true },
 --   incremental_selection = {
 --     enable = true,
 --     keymaps = {
