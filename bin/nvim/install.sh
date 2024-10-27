5aaf6ef20cbb6186437e746a4d82447fbf6c1166
Vladyslav Bohaichuk <bohaichuk.v@gmail.com>
2024-10-27 17:58:49 +0200

    env: merge home env files

---
+++ bin/nvim/install.sh
@@ -0,0 +1,5 @@
+cd $HOME/bin/_nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+ln -s $HOME/bin/nvim/nvim.appimage $HOME/.local/bin/nvim
+
